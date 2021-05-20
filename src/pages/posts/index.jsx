718e5c5f1cc29b975d727bf857b1b438c3abba53
Gabriel Zilmar <51098008+GabrielZilmar@users.noreply.github.com>
2021-05-20 08:43:43 -0300

    Feature/card (#1)
    * feat: init post cards
    
    * feat: request api posts

---
+++ src/pages/posts/index.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { list } from '../../api/posts';
+
+import Card from '../../layout/Card/Card';
+
+import './index.css';
+
+export default () => {
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  const handlePosts = async () => {
+    setLoading(true);
+    const response = await list();
+    setPosts(response.length > 0 ? response : []);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    handlePosts();
+  }, []);
+
+  return (
+    <div className="post">
+      {
+        !loading ? (
+          posts.map((post, index) => (
+            <div style={{ margin: index === 0 && '1% auto 1.5% auto' }}>
+              <Card title={post.title}>
+                <p key={post.id}>{post.body}</p>
+              </Card>
+
+            </div>
+          ))
+
+        ) : (
+          <Card title="Teste">
+            <h1 key={0}>Loading...</h1>
+          </Card>
+        )
+      }
+
+    </div>
+  );
+};

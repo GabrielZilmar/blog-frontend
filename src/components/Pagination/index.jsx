471191f79e8500132ac9372ccc4403056b7d2fe3
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2021-05-24 15:46:55 -0300

    feat: paginate posts

---
+++ src/components/Pagination/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+  const items = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i += 1) {
+    items.push(i);
+  }
+
+  return (
+    <nav>
+      <ul className="pagination" style={{ display: 'flex', justifyContent: 'center', overflow: 'auto' }}>
+        {items.map((number) => (
+          <li key={number} className="page-item">
+            <a onClick={() => paginate(number)} href="!#" className="page-link">
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+Pagination.propTypes = {
+  postsPerPage: PropTypes.number.isRequired,
+  totalPosts: PropTypes.number.isRequired,
+  paginate: PropTypes.func.isRequired,
+};
+
+export default Pagination;
